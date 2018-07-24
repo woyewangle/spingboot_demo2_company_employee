a54002730b88ebbb7528be756aa4f54de529cbbd
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-07-24 18:56:03 +0800

    添加对象模型

---
+++ src/main/java/com/example/demo/entity/Company.java
@@ -0,0 +1,45 @@
+package com.example.demo.entity;
+
+/**
+ * @Author: 余锡鸿
+ * @Description:
+ * @Date: Create in 6:48 PM 7/24/2018
+ * @Modified By:
+ */
+public class Company {
+    public int id;
+    public String name;
+    public int employeesNumber;
+
+    public Company(){}
+
+    public Company(int id, String name, int employeesNumber) {
+        this.id = id;
+        this.name = name;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+}
---
+++ src/main/java/com/example/demo/entity/Employees.java
@@ -0,0 +1,65 @@
+package com.example.demo.entity;
+
+/**
+ * @Author: 余锡鸿
+ * @Description:
+ * @Date: Create in 6:52 PM 7/24/2018
+ * @Modified By:
+ */
+public class Employees {
+    public int id;
+    public String name;
+    public int age;
+    public String gender;
+    public int salary;
+
+    public Employees(){}
+
+    public Employees(int id, String name, int age, String gender, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
